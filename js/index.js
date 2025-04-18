ae675119699550303036a89b26e47d016b2d39d7
sadhin28 <taosifsadhin113@gmail.com>
2025-04-19 00:53:15 +0600

    call data andd show

--- js/index.js
+++ js/index.js
@@ -1,7 +1,16 @@
 
 function todos(){
-    fetch('https://jsonplaceholder.typicode.com/todos')
+    fetch('https://jsonplaceholder.typicode.com/users')
     .then(res => res.json())
-    .then(data=>console.log(data))
+    .then(data=>displayTodos(data))
 }
    
+function displayTodos(data){
+      const ul =document.getElementById('users-list')
+
+   for(const user of data){
+     const li=document.createElement('li');
+     li.innerHTML=user.name;
+     ul.appendChild(li);
+   }
+}
