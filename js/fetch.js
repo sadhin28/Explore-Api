e9f88f29a71dcaf4be7bc46e06e467e204f507f8
sadhin28 <taosifsadhin113@gmail.com>
2025-04-19 17:25:08 +0600

    practic with api

--- js/fetch.js
+++ js/fetch.js
@@ -3,10 +3,15 @@ function  loadData(){
     fetch('https://jsonplaceholder.typicode.com/todos/1')
     .then(response => response.json())
     .then(json => console.log(json))
+    .catch(error => console.error('error happend',error))
 }
 
 const loadComent = async()=>{
+   try{
     const res = await fetch('https://jsonplaceholder.typicode.com/todos/1');
     const data = await res.json();
     console.log(data)
+   }catch(error){
+      console.error('Data load error')
+   }
 }
