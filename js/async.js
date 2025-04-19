bd284cb2caf1745169a5ad7e3514b49b9d51d4d2
sadhin28 <taosifsadhin113@gmail.com>
2025-04-19 21:17:12 +0600

    async practice

---
+++ js/async.js
@@ -0,0 +1,11 @@
+const myLoader = ( )=>{
+    return new Promise((resolve,reject)=>{
+        const success = Math.random();
+        if(success <= 0.5){
+            resolve(success)
+        }
+        else{
+            reject(success)
+        }
+    })
+}
